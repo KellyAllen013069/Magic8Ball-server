3c32ad7c655c2916e033932da54cb8a9e99398c5
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2023-04-02 15:14:00 -0500

    updating

--- db/connection.js
+++ db/connection.js
@@ -2,5 +2,6 @@ import mysql from "mysql";
 import config from "../config/index.js";
 
 const connection = mysql.createPool(config.mysql);
+console.log("connection is " + connection);
 
 export default connection;
