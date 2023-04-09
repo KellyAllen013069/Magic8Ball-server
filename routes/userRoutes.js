79f614f64abf375a4fd5a9a1e1c85643e0a4b079
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2023-04-09 17:03:38 -0500

    updating 4

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,21 +3,25 @@ import express from 'express';
 const userRouter = express.Router();
 
 userRouter.get('/getUser', (req, res) => {
+  try {
     console.log("*************in getuser**************");
     console.log("req is " + req.user);
     if(req.user) {
-        console.log("user is " + req.user[0]);
-        res.status(200).json({
-            success: true,
-            message: "successfull",
-            user: req.user[0]
-            
-        })
+      console.log("user is " + req.user[0]);
+      res.status(200).json({
+        success: true,
+        message: "successfull",
+        user: req.user[0]
+      })
     } else {
-        console.log("user not found");
-        res.json({success: false, message: "unsuccessful"})
+      console.log("user not found");
+      res.json({success: false, message: "unsuccessful"})
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: "server error" });
+  }
+});
 
 userRouter.get('/logout', (req, res, next) => {
   console.log("**********************************IN LOGOUT******************************");
