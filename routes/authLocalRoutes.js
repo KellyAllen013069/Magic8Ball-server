4d66ec89229553a9956cd836045d09e99d396289
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2023-08-07 11:23:58 -0500

    change to auth local

--- routes/authLocalRoutes.js
+++ routes/authLocalRoutes.js
@@ -23,7 +23,7 @@ passport.use(localStrategy)
 
 authLocalRouter.post('/login', passport.authenticate('local', {
   failureFlash: true,
-  failureRedirect:'https://magic8ballserver.onrender.com/api/authlocal/login-failure'}), (req, res) => {
+  failureRedirect:'https://magic8ballclient.onrender.com/api/authlocal/login-failure'}), (req, res) => {
   res.redirect('https://magic8ballclient.onrender.com/api/authlocal/login-success');
 });
 
